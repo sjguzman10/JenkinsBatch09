838f304b175461e34c6f7fc578cf0c2d1ac2aaaf
sjguzman10 <stephanie.j.guzman@gmail.com>
2021-07-23 21:28:05 -0700

    "friday push03"

--- src/com/collection/Main.java
+++ src/com/collection/Main.java
@@ -3,8 +3,7 @@ package com.collection;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
-        for (int i=0; i<6; i++) {
-
+        for (int i=0; i<5; i++) {
             System.out.println("Batch 09 is great!");
             Thread.sleep(1000);
         }
